846b1c240f69354514f1b3de92664526d9845844
Dmitry Baranovsky <dmitry.baranovsky.alexeevich@gmail.com>
2018-11-21 17:24:19 +0300

    Rewrote the program to allow specifying the name of the file and to treat \r as an empty character.

--- HW4/HW4.3.cpp
+++ HW4/HW4.3.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
 
+int const stringLength = 256;
+
 int main()
 {
-	FILE* file = fopen("input.txt", "r");
+	char* name = new char[stringLength];
+	printf("Enter the name of the file: ");
+	scanf("%255s", name);
+	FILE* file = fopen(name, "r");
+	delete[] name;
 	int amountOfNonEmptyLines = 0;
 	bool isEmpty = true;
 	char character = '\0';
@@ -17,7 +23,7 @@ int main()
 		{
 			continue;
 		}
-		if ((character != '\t') && (character != ' '))
+		if ((character != '\t') && (character != ' ') && (character != '\r'))
 		{
 			isEmpty = false;
 			amountOfNonEmptyLines++;
