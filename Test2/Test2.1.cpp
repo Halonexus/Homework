7674628fff30b218255a71fc499a33c9ef154a15
Dmitry Baranovsky <dmitry.baranovsky.alexeevich@gmail.com>
2018-12-17 17:29:20 +0300

    Update Test2.1.cpp
    Added required spaces

--- Test2/Test2.1.cpp
+++ Test2/Test2.1.cpp
@@ -7,7 +7,7 @@ int fibonacciNumber(int index)//0 1 1 2...
 	{
 		tempNumber[(i + 1) % 2] = tempNumber[0] + tempNumber[1];
 	}
-	return tempNumber[(index+1)%2];
+	return tempNumber[(index + 1) % 2];
 }
 
 int main()
@@ -15,6 +15,6 @@ int main()
 	printf("Enter the index of the fibonacci number: ");
 	int index = 0;
 	scanf("%d", &index);
-	printf("%dth fibonacci number: %d\n",index, fibonacciNumber(index));
+	printf("%dth fibonacci number: %d\n", index, fibonacciNumber(index));
 	return 0;
 }
