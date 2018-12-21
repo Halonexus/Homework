83877a977ca7717a422cab94b25e681886bff5d9
Dmitry Baranovsky <dmitry.baranovsky.alexeevich@gmail.com>
2018-12-21 18:02:39 +0300

    Changed input to file

--- Test3/Test3.3/Test3.3.cpp
+++ Test3/Test3.3/Test3.3.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include "AStar.h"
 
+int const stringLength = 256;
+
 void createAdjacencyMatrix(int**, bool**&, int, int);
+void getMatrix(int**& matrix, int rows, int columns, FILE* file);
 
 int main()
 {
@@ -11,54 +14,59 @@ int main()
 	scanf("%d", &rows);
 	printf("Enter the amount of columns: ");
 	scanf("%d", &columns);
-	printf("Enter the elements of the matrix: \n");
-	int** matrix = new int*[rows];
-	bool** result = new bool*[rows];
-	for (int i = 0; i < rows; i++)
+	printf("Enter the filename: ");
+	char* filename = new char[stringLength];
+	scanf("%s", filename);
+	FILE* file = fopen(filename, "r");
+	if (!file)
 	{
-		result[i] = new bool[rows] {0};
-		matrix[i] = new int[columns];
-		for (int j = 0; j < columns; j++)
-		{
-			int value = 0;
-			scanf("%d", &value);
-			matrix[i][j] = value;
-		}
+		printf("Error opening file.\n");
 	}
-
-	createAdjacencyMatrix(matrix, result, rows, columns);
-	for (int i = 0; i < rows; i++)
+	else
 	{
-		for (int j = 0; j < rows; j++)
+		int** matrix = new int*[rows];
+		bool** result = new bool*[rows];
+		for (int i = 0; i < rows; i++)
 		{
-			printf("%d", result[i][j]);
+			result[i] = new bool[rows] {0};
+			matrix[i] = new int[columns];
 		}
-		printf("\n");
-	}
-	for (int i = 0; i < rows; i++)
-	{
-		int count = 0;
-		Node* end = new Node{ i,0,0,0,nullptr };
-		for (int j = 0; j < rows; j++)
+		getMatrix(matrix, rows, columns, file);
+		for (int i = 0; i < rows; i++)
 		{
-			Node* start = new Node{ j,0,0,0,nullptr };
-			if (aStar(start, end, result, rows))
+			for (int j = 0; j < columns; j++)
 			{
-				count++;
+				printf("%d ", matrix[i][j]);
+			}
+			printf("\n");
+		}
+		createAdjacencyMatrix(matrix, result, rows, columns);
+		for (int i = 0; i < rows; i++)
+		{
+			int count = 0;
+			Node* end = new Node{ i,0,0,0,nullptr };
+			for (int j = 0; j < rows; j++)
+			{
+				Node* start = new Node{ j,0,0,0,nullptr };
+				if (aStar(start, end, result, rows))
+				{
+					count++;
+				}
+			}
+			if (count == rows)
+			{
+				printf("Point %d is reachable from every point.\n", i);
 			}
 		}
-		if (count == rows)
+		for (int i = 0; i < rows; i++)
 		{
-			printf("Point %d is reachable from every point.\n", i);
+			delete[] matrix[i];
+			delete[] result[i];
 		}
+		delete[] matrix;
+		delete[] result;
 	}
-	for (int i = 0; i < rows; i++)
-	{
-		delete[] matrix[i];
-		delete[] result[i];
-	}
-	delete[] matrix;
-	delete[] result;
+	
 	return 0;
 }
 
@@ -79,6 +87,36 @@ void createAdjacencyMatrix(int** matrix, bool**& adjMatrix, int rows, int column
 						break;
 					}
 				}
+			}
+		}
+	}
+	return;
+}
+
+void getMatrix(int**& matrix, int rows, int columns, FILE* file)
+{
+	char character[2] = { '\0' };
+	int i = 0;
+	while (!feof(file) && i < rows * columns)
+	{
+		fgets(character, 2, file);
+		if (character[0] == '0')
+		{
+			matrix[i / columns][i % columns] = 0;
+			i++;
+		}
+		else if (character[0] == '1')
+		{
+			matrix[i / columns][i % columns] = 1;
+			i++;
+		}
+		else if (character[0] == '-')
+		{
+			fgets(character, 2, file);
+			if (character[0] == '1')
+			{
+				matrix[i / columns][i % columns] = -1;
+				i++;
 			}
 		}
 	}
