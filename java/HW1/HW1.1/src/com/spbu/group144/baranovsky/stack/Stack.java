ce88282473e8816bf6884f042b163c416e376c6f
Dmitry Baranovsky <dmitry.baranovsky.alexeevich@gmail.com>
2019-04-12 04:39:26 +0300

    Added spaces, removed uiDesigner file.

---
+++ java/HW1/HW1.1/src/com/spbu/group144/baranovsky/stack/Stack.java
@@ -0,0 +1,39 @@
+package com.spbu.group144.baranovsky.stack;
+
+/** A stack for storing integer values.*/
+
+public class Stack {
+    private StackElement head;
+
+    /**
+     * Removes an element from the top of the stack.
+     *
+     * @return Returns the integer value of the top element.
+     */
+    public int pop() {
+        if (!isEmpty()) {
+            StackElement top = head;
+            head = head.getNext();
+            return top.getValue();
+        }
+        return 0;
+    }
+
+    /**
+     * Adds a new element to the stack.
+     *
+     * @param value The integer value of the new element.
+     */
+    public void push(int value) {
+        head = new StackElement(value, head);
+    }
+
+    /**
+     * Checks whether the stack is empty.
+     *
+     * @return True if the stack is empty. False otherwise.
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
