ae175856739229b2d693dfa720d8a328c9b48f7e
Baranovsky Dmitry <dmitry.baranovsky.alexeevich@gmail.com>
2019-11-19 14:35:05 +0300

    Added HW5.1

---
+++ java/HW5/HW5.1/src/main/java/ru/spbu/baranovsky/dmitry/Controller.java
@@ -0,0 +1,21 @@
+package ru.spbu.baranovsky.dmitry;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+
+public class Controller {
+    @FXML
+    private Slider slider = new Slider();
+
+    @FXML
+    private ProgressBar progressBar;
+
+    /** A method that changes progress bar according to slider's changes */
+    public void initialize() {
+        slider.setMin(0);
+        slider.setMax(1);
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> progressBar.progressProperty().setValue(newValue));
+    }
+}
