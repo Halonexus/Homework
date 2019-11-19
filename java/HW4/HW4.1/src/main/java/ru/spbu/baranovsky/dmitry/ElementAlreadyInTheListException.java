544e8882e88e46820aa6762043a16d946726891a
Baranovsky Dmitry <dmitry.baranovsky.alexeevich@gmail.com>
2019-11-19 14:31:16 +0300

    Added HW4.1

---
+++ java/HW4/HW4.1/src/main/java/ru/spbu/baranovsky/dmitry/ElementAlreadyInTheListException.java
@@ -0,0 +1,10 @@
+package ru.spbu.baranovsky.dmitry;
+
+/** The exception that throwed if user is trying to add element that's already in the list */
+public class ElementAlreadyInTheListException extends Exception {
+
+    public ElementAlreadyInTheListException() { super(); }
+
+    public ElementAlreadyInTheListException(String msg) { super(msg); }
+
+}
---
+++ java/HW4/HW4.1/src/main/java/ru/spbu/baranovsky/dmitry/ElementIsNotInTheListException.java
@@ -0,0 +1,9 @@
+package ru.spbu.baranovsky.dmitry;
+
+/** The exception that throwed if user is trying to remove element that's not in the list */
+public class ElementIsNotInTheListException extends Exception {
+
+    public ElementIsNotInTheListException() { super(); }
+
+    public ElementIsNotInTheListException(String msg) { super(msg); }
+}
